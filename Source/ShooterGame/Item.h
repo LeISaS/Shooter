4b1cfaa054f5455c4fb3e8d816ee989a1cc1bd61
윤상윤 <yk19888@naver.com>
2022-01-13 18:07:13 +0900

    [update] WidgetBP

--- Source/ShooterGame/Item.h
+++ Source/ShooterGame/Item.h
@@ -32,6 +32,10 @@ private:
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Item Properties",meta = (AllowPrivateAccess = "true"))
 	class UBoxComponent* CollisionBox;
 
+	/** Popup Widget for when the player looks at the item */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
+
 
 public :
 
